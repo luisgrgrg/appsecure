5cfb0848d204a1b536722f98c9e029fa9ebdfa3b
luisgrgrg <luisgarogu@gmail.com>
2024-12-27 10:54:26 -0500

    FD10

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,4 @@
 "use client"
-import "../app/globals.css";
 
 export function Login() {
 
@@ -9,29 +8,30 @@ export function Login() {
             <div></div>
 
             {/* Columna Central con Tarjeta Negra */}
-            <div className="bg-gradient-to-r from-mainColor to-mainColorDeg w-72 shadow rounded-[30px] p-4 flex flex-col items-center pt-10">
+            <div className="bg-gradient-to-r from-mainColor to-mainColorDeg w-72 min-h-[250px] shadow rounded-30  p-4 flex flex-col items-center pt-10">
                 {/* Espacio Superior (Espacio Estético) */}
                 <img className="rounded-t-lg" src="/images/icon-secureobyco.png" alt="" />
                 {/* Tarjeta INTERNA */}
-                <div className="bg-mainColor2 bg-opacity-5 rounded-[30px] shadow p-4 w-72 h-[445px] flex flex-col items-center justify-start relative">
+                <div className="bg-mainColor2 bg-opacity-005 rounded-30 shadow p-4 w-72 h-[305px] mt-5 pt-10">
                     <h1 className="text-center text-white mb-4 text-2xl">OBYSecure</h1>
-                    <form className="place-items-center">
+                    <form className="w-full place-items-center">
                         <div className="mb-5">
                             <input
                                 type="email"
                                 id="email"
-                                className="bg-mainColor2 sm:mx-4 bg-opacity-1  text-sm text-gray-100 rounded-lg 
-                             focus:ring-blue-500 focus:border-001 block p-2.5"
+                                className="bg-mainColor2 bg-opacity-1 text-sm text-gray-100 rounded-lg 
+                             focus:ring-blue-500 focus:border-001 block  p-2.5"
                                 placeholder="usuario"
-                                required 
+                                required
                             />
                         </div>
-                        <div className="mb-5"> 
+                        <div className="mb-5">
                             <input
                                 type="password"
                                 id="password"
-                                className="bg-mainColor2 sm:mx-4 bg-opacity-1  text-gray-100 text-sm rounded-lg 
-                             focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
+                                className="bg-mainColor2 bg-opacity-1 text-gray-100 text-sm rounded-lg 
+                             focus:ring-blue-500 focus:border-blue-500 block p-2.5 
+                            "
                                 placeholder="contraseña"
                                 required
                             />
@@ -54,5 +54,3 @@ export function Login() {
         </div>
     )
 }
-
-export default Login
